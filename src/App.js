67fce37b59cdc58b3d99cc64eba6760c913adc7b
AnnaxT <82735488+AnnaxT@users.noreply.github.com>
2022-03-08 23:02:56 +0000

    remove unused code

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ import Quote from './components/Quote';
 import Container from './components/Container';
 import Links from "./components/Links";
 import BottomRow from "./components/BottomRow";
-import { purple, blue } from '@mui/material/colors'
 
 const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
-
-
 
 
 function App() {
